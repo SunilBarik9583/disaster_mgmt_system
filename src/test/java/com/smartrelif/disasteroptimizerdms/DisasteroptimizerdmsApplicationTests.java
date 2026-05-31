e64993ba37c717176c9e2b6a12c53902a98645de
SunilBarik <132165142+SunilBarik9583@users.noreply.github.com>
2026-05-31 17:34:43 +0530

    Update DisasteroptimizerdmsApplicationTests.java

--- src/test/java/com/smartrelif/disasteroptimizerdms/DisasteroptimizerdmsApplicationTests.java
+++ src/test/java/com/smartrelif/disasteroptimizerdms/DisasteroptimizerdmsApplicationTests.java
@@ -2,12 +2,18 @@ package com.smartrelif.disasteroptimizerdms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootTest
+@SpringBootTest(
+    classes = DisasteroptimizerdmsApplication.class,
+    properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+    }
+)
 class DisasteroptimizerdmsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
